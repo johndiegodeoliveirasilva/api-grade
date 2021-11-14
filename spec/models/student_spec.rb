51092fa24ba9be2a05258cc8da1abe4f47d20479
John Diego <johnoliveirasilva9@gmail.com>
2021-11-14 13:31:43 -0200

    Create Rspec Student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,8 +3,20 @@ require 'rails_helper'
 RSpec.describe Student, type: :model do
   
   context 'new Student' do
-    it 'student with a valid email should be valid'
-      student = build(:students)
-      expect(student).valid
+    it 'student with a valid email should be valid' do
+      student = build(:student)
+      expect(student).to be_valid
+    end
+
+    it 'student with invalid email should be invalid' do
+      student = build(:student, :without_email)
+      expect(student).not_to be_valid
+    end
+
+    it 'student with taken email should be invalid' do
+      other_student = create(:student)
+      student = build(:student, email: other_student.email)
+      expect(student).not_to be_valid
+    end
   end
 end
